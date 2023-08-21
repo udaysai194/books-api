9a44472c6107393d9f15bc60a1213396aba7e5ce
uday sai Vemula <udaysai194@gmail.com>
2023-08-21 16:34:19 +0530

    first commit from windows

--- main.go
+++ main.go
@@ -3,53 +3,29 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
-	"os"
 
 	"books-api/models"
 	"books-api/storage"
 
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-	"gorm.io/gorm"
 )
 
-type Repository struct {
-	DB *gorm.DB
-}
-
-func handleError(err error, msg string) {
+func HandleError(err error, msg string) {
 	if err != nil {
 		fmt.Println(msg)
 		log.Fatal(err)
 	}
 }
 
-func configure(envFile string) storage.Config {
-	err := godotenv.Load(envFile)
-	handleError(err, "could not load .env file")
-
-	config := &storage.Config{
-		Host:     os.Getenv("DB_HOST"),
-		Port:     os.Getenv("DB_PORT"),
-		Password: os.Getenv("DB_PASS"),
-		User:     os.Getenv("DB_USER"),
-		DBName:   os.Getenv("DB_NAME"),
-		SSLMode:  os.Getenv("DB_SSLMODE"),
-	}
-
-	return config
-}
-
 func main() {
 
-	config := configure(".env")
+	config := Configure("UdayVM2.env")
 
 	db, err := storage.Connect(config)
-	handleError(err, "could not connect to the database")
+	HandleError(err, "could not connect to the database")
 
 	err = models.MigrateBooks(db)
-	handleError(err, "could not migrate")
+	HandleError(err, "could not migrate")
 
 	r := Repository{
 		DB: db,
@@ -59,24 +35,4 @@ func main() {
 	r.SetupRoutes(router)
 	router.Run("localhost:8080")
 
-}
-
-func (r *Repository) SetupRoutes(router *gin.Engine) {
-	router.POST("/add-book", r.AddBook)
-	router.GET("/books", r.GetBooks)
-}
-
-func (r *Repository) GetBooks(c *gin.Context) {
-	bookModels := &[]models.Book{}
-	err := r.DB.Find(&bookModels)
-	handleError(err.Error, "no books found in database")
-	c.JSON(http.StatusOK, bookModels)
-}
-
-func (r *Repository) AddBook(c *gin.Context) {
-	book := models.Book{}
-	err := c.BindJSON(&book)
-	handleError(err, "cant bind the books")
-	err = r.DB.Create(&book).Error
-	handleError(err, "cant POST books")
 }
